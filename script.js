ba13f04c7462bace5954fb7c91c4bd4099590916
HuntOreo <hloreair@outlook.com>
2025-07-01 17:51:01 -0400

    Console can make choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+// This is a game that will be played via the console.
+// It will take a players input and pin it against the computers input.
+// An algorithm will check to see who wins or if there is a draw.
+
+// Computer generates 1 of 3 choices: Rock, Paper, Scissors.
+//  The computer will pick 1 of these choices at random.
+//   Then the computer will output their choice onto the console.
+
+function getComputerChoice() {
+
+  const chance = Math.random();
+
+  if (chance >= 0.66) {
+    return 'rock';
+  } else if (chance <= 0.65 && chance > 0.33) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+
+};
+
+console.log(getComputerChoice());
