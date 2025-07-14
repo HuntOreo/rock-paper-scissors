c3c6fb22f794ab3e7e09e57d23ab9a339b6882f0
HuntOreo <hloreair@outlook.com>
2025-07-13 22:55:18 -0400

    Declare game outcome functions

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// This is a game that will be played via the console.
-// It will take a players input and pin it against the computers input.
-// An algorithm will check to see who wins or if there is a draw.
-// The game will also keep track of scores between the computer and the player.
+
 
 // Global variables.
 let humanScore = 0;
@@ -22,10 +19,6 @@ function getComputerChoice() {
   }
 };
 
-// A prompt will ask for a players choice
-//  The player must provide a choice between Rock, Paper, Scissors.
-//   If a choice is given outside of the scope, an error is provided.
-//    The console will provide feedback to the player by logging the input.
 function getPlayerChoice(playerChoice) {
 
   if (typeof (playerChoice) === undefined) {
@@ -51,43 +44,60 @@ function getPlayerChoice(playerChoice) {
   }
 }
 
-// Receive the player and computer choices.
-//  Compare the choices and determine who is a victor.
-//   If there is no victor, declare a draw.
-//    Ask the player if they wish to go again.
+
 function playRound(humanChoice, computerChoice) {
 
-  if (humanChoice == 'rock') { // Rock
-    if (computerChoice == 'paper') {
-      computerScore++;
-      return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
-    } else if (computerChoice == 'scissors') {
-      humanScore++;
-      return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
-    } else {
-      return `Draw \n Both players chose ${humanChoice}`;
-    }
-  } else if (humanChoice == 'paper') { // Paper
-    if (computerChoice == 'rock') {
-      humanScore++;
-      return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
-    } else if (computerChoice == 'scissors') {
-      computerScore++;
-      return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
-    } else {
-      return `Draw \n Both players chose ${humanChoice}`;
-    }
-  } else { // Scissors
-    if (computerChoice == 'rock') {
-      computerScore++;
-      return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
-    } else if (computerChoice == 'paper') {
-      humanScore++;
-      return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
-    } else {
-      return `Draw \n Both players chose ${humanChoice}`;
-    }
+  if (humanChoice === '🪨') {
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
   }
+
+  else if (humanChoice === '📄') {
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
+
+  }
+
+  else if (humanChoice === '✂️') {
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
+    if (computerChoice === '🪨') { }
+
+  }
+
+  // if (humanChoice == 'rock') { // Rock
+  //   if (computerChoice == 'paper') {
+  //     computerScore++;
+  //     return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
+  //   } else if (computerChoice == 'scissors') {
+  //     humanScore++;
+  //     return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
+  //   } else {
+  //     return `Draw \n Both players chose ${humanChoice}`;
+  //   }
+  // } else if (humanChoice == 'paper') { // Paper
+  //   if (computerChoice == 'rock') {
+  //     humanScore++;
+  //     return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
+  //   } else if (computerChoice == 'scissors') {
+  //     computerScore++;
+  //     return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
+  //   } else {
+  //     return `Draw \n Both players chose ${humanChoice}`;
+  //   }
+  // } else { // Scissors
+  //   if (computerChoice == 'rock') {
+  //     computerScore++;
+  //     return `Computer Wins \n ${computerChoice} beats ${humanChoice}`;
+  //   } else if (computerChoice == 'paper') {
+  //     humanScore++;
+  //     return `Player Wins \n ${humanChoice} beats ${computerChoice}`;
+  //   } else {
+  //     return `Draw \n Both players chose ${humanChoice}`;
+  //   }
+  // }
 }
 
 // Will play 5 rounds in total.
@@ -147,8 +157,19 @@ function playGame(playerChoice) {
   }
 };
 
+function declareComputerWins(computerChoice, playerChoice) {
+
+}
+
+function declarePlayerWins(playerChoice, computerChoice) {
+
+}
+
+function declareDraw(playerChoice, computerChoice) {
+
+}
+
 const btns = document.querySelectorAll('button');
 btns.forEach(elem => {
   elem.addEventListener('click', playGame(elem.innerText))
 })
-// playGame();
